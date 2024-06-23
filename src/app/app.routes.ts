e5c222d8ed9eb8b9bb3d4a639901a51927269a83
Arun <arunjothi2599@gmail.com>
2024-06-23 19:47:23 +0530

    Content section developed with responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+export const routes: Routes = [
+    { path: 'main', component: MainComponent },
+];
